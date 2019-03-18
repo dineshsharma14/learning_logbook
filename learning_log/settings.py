ae5e396c173c96b5775f0620e4ed83d54613ac86
dsharma <dinesh.sw3@gmail.com>
2019-03-18 12:37:40 -0500

    Set DEBUG=False for Heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -148,8 +148,9 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     
     # Only allow heroku to host the project.
-    ALLOWED_HOSTS = ['*']
-    #DEBUG = True
+    ALLOWED_HOSTS = ['learning-logbook.herokuapp.com']
+
+    DEBUG = False
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
